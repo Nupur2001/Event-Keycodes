c2a93b4eaf8df3e3e4c9cb86f5957742ff9b1cce
Nupur Sikka <67430495+Nupur2001@users.noreply.github.com>
2025-02-14 10:37:16 +0530

    Update script.js

--- script.js
+++ script.js
@@ -5,16 +5,16 @@ document.addEventListener("DOMContentLoaded", () => {
   let eventKey = document.querySelector(".eventKey");
   let eventKeyCode = document.querySelector(".eventKeyCode");
   let eventCode = document.querySelector(".eventCode");
-  let randomNumber=Math.floor(Math.random()*100)+1
-  let randomAlphabet=Math.floor(Math.random()*26)+97
-  
 
   document.addEventListener("keydown",(e)=>{
     allEventCodes.classList.add('active')
-    pressAnyKeyContainer.classList.remove('active')
-    eventKey.innerHTML=(String.fromCharCode(randomAlphabet))//event.key
-    eventKeyCode.innerHTML=(randomNumber) //event.keyCode
+    pressAnyKeyContainer.style.display="none"
+    eventKey.innerHTML=(e.key)//event.key
+    eventKeyCode.innerHTML=(e.keyCode) //event.keyCode
     eventCode.innerHTML=(e.code)//event.code
+    if (e.key===" ") {
+      eventKey.innerHTML="Space"
+    }
   }
     )
 });
